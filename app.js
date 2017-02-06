21d0573422904512519b92efacc96478508292dc
tgzzl <1257408774@qq.com>
2017-02-06 11:53:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,13 @@ App({
     if(this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
     }else{
-      //调用登录接口
       wx.login({
         success: function (res) {
           var code = res.code;
           wx.getUserInfo({
             success: function (res) {
-              wx.request({
-                url: 'http://localhost:8888/index?code='+code+'&encryptedData='+res.encryptedData+'&iv='+res.iv,
-                success: function (decryptData) {
-                  console.log('decryptData',JSON.stringify(res), JSON.stringify(decryptData));
-                }
-              });
+              wx.request({url: 'http://localhost:8888/createwxaqrcode'})
+              wx.request({url: 'http://localhost:8888/index?code='+code+'&encryptedData='+res.encryptedData+'&iv='+res.iv})
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
