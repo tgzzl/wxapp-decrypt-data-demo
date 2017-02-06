2ac0c58002fee37a62298ca3d14b580a0b411f71
tgzzl <1257408774@qq.com>
2017-02-06 11:52:59 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var https = require('https');
+var request = require('request');
 var url = require("url");
+var fs = require("fs");
 var querystring = require('querystring');
 var WXBizDataCrypt = require('./WXBizDataCrypt');
 var appId = '请替换成您的appId';
@@ -21,6 +23,8 @@ function start(route) {
           var data = decryptData(params.code, params.encryptedData, params.iv);
           response.write(data);
         }
+    }else if('/createwxaqrcode' == pathname){
+        createwxaqrcode();
     }else{
         response.write(JSON.stringify(_url));
     }
@@ -35,22 +39,21 @@ function decryptData(code,encryptedData,iv) {
   function decode(d){
     return d.replace(/\s+/g,'+')
   }
-  
+
   var data = ''
 
-  var url = 'https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&appid='
-  +appId+'&secret='+secret+'&js_code='+code;
+  var url = 'https://api.weixin.qq.com/sns/jscode2session?appid='+appId+'&secret='+secret+'&js_code='+decode(code)+'&grant_type=authorization_code';
   https.get(url, (res) => {
         var body = '';
         res.on('data', (d) => { body += d })
            .on('end', () => { 
                body = JSON.parse(body);
-               var sessionKey = body.session_key;
-          
+               var sessionKey = body.session_key
+               
                sessionKey = decode(sessionKey)
                encryptedData = decode(encryptedData)
                iv = decode(iv)
-               
+
                var pc = new WXBizDataCrypt(appId, sessionKey)
                data = pc.decryptData(encryptedData, iv)
 
@@ -61,6 +64,61 @@ function decryptData(code,encryptedData,iv) {
     });
 
   return data;
+}
+
+function createwxaqrcode(){
+  https.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+appId+'&secret='+secret, (res) => {
+        var body = '';
+        res.on('data', (d) => { body += d })
+           .on('end', () => { 
+               body = JSON.parse(body);
+               console.log('token', body)
+               post_createwxaqrcode(body.access_token)
+               request_createwxaqrcode(body.access_token)
+            });
+    });
+
+  /*  use origin https request  */
+  function post_createwxaqrcode(token){
+    var post_data = JSON.stringify({
+      'path': 'pages/map/map'
+    });
+    var options = {
+      host: 'api.weixin.qq.com',
+      port: '443',
+      path: '/cgi-bin/wxaapp/createwxaqrcode?access_token='+token,
+      method: 'POST',
+      headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': post_data.length
+        }
+    };
+    var req = https.request(options, (res) => {
+      var body = '';
+      res.setEncoding('binary');
+      res.on('data', (d) => { body += d; });
+      res.on('end', () => {
+        fs.writeFile('./https_'+Date.now()+'.jpg', body, 'binary', (err) => { console.log('writeFile', err) } )
+      });
+    });
+    req.write(post_data);
+    req.end();
+  }
+
+  /*  use request module  */
+  function request_createwxaqrcode(token){
+    var options = {
+      uri: 'https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token='+token,
+      method: 'POST',
+      json: {'path': 'pages/map/map'},
+      encoding: 'binary'
+    };
+    request(options, (error, res, body) => {
+      if(!error && res.statusCode == 200){
+        fs.writeFile('./request_'+Date.now()+'.jpg', body, 'binary', (err) => { console.log('writeFile', err) } )
+      }
+    });
+  }
 }
 
 exports.start = start;
